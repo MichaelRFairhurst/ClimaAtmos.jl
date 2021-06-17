5e4015a36980d96b083ec40c66004bbc00ef2887
Documenter.jl <documenter@juliadocs.github.io>
2021-06-17 18:30:13 +0000

    build based on 7758dab

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"function_index/#List-of-functions-in-Aleph.jl-module","page":"Function Index","title":"List of functions in Aleph.jl module","text":"","category":"section"},{"location":"function_index/","page":"Function Index","title":"Function Index","text":"Modules = [Aleph]","category":"page"},{"location":"#Aleph.jl","page":"Home","title":"Aleph.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"A repository for the development of interfaces in the CLIMA project. The main objective is to","category":"page"},{"location":"","page":"Home","title":"Home","text":"Have a unified abstraction layer for different computational backends. \nmake high performance computing accessible","category":"page"}]
+}
